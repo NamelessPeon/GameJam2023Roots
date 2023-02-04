301a8a60dbd80c0415678f9ed1a331906949c759
fahreradam <72369125+fahreradam@users.noreply.github.com>
2023-02-04 15:32:20 -0500

    Finished tile spawner

--- Assets/Scripts/TileMaker.cs
+++ Assets/Scripts/TileMaker.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class TileMaker : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject tile;
+    public int tileWidth;
+    public int tileHeight;
+    public int tileDepth;
+    public int NumRow;
+    public int NumCol;
     void Start()
     {
-        
+        for (int i = 0; i < NumCol; i++)
+            for (int j = 0; j < NumRow; j++)
+            {
+                GameObject NewTile = GameObject.Instantiate(tile);
+                NewTile.transform.localScale = new Vector3(tileWidth, tileHeight, tileDepth);
+                NewTile.transform.position = new Vector3(transform.position.x + (j * tileWidth), transform.position.y + (i * tileHeight), transform.position.z);
+            }
+
     }
 
     // Update is called once per frame
