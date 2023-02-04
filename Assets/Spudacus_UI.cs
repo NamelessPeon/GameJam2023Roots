5c52dd6fdd888ff6334f807ea06d35294a85e497
fahreradam <72369125+fahreradam@users.noreply.github.com>
2023-02-04 10:57:26 -0500

    finished the initial countdown sequence for the spudacus scene and linked it to starting the mini game

--- Assets/Spudacus_UI.cs
+++ Assets/Spudacus_UI.cs
@@ -5,16 +5,30 @@ using TMPro;
 
 public class Spudacus_UI : MonoBehaviour
 {
-    public TextMesh CountDown;
+    public TextMeshProUGUI CountDown;
+    public Spawn_Controller SpawnController;
+
+    float time = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        CountDown.text = "Here";
+        CountDown.text = time.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            CountDown.text = System.Math.Round(time, 2).ToString();
+            if (time < 0)
+            {
+                CountDown.text = "0";
+                SpawnController.Can_Spawn = true;
+                CountDown.gameObject.SetActive(false);
+            }
+            
+        }
     }
 }
