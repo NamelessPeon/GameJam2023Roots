e8e8b607724bb9eab9fb076d1cd40e438c5bd7d1
GoldenKnight582 <tycobb582@gmail.com>
2023-02-05 15:28:53 -0500

    Sound Effects Added

--- Assets/Scripts/SpudacusPlayer.cs
+++ Assets/Scripts/SpudacusPlayer.cs
@@ -82,9 +82,11 @@ public class SpudacusPlayer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Potato_Projectile(Clone)" && curShield == shieldType.Potato)
+        if ((other.gameObject.name == "Potato_Projectile(Clone)" && curShield == shieldType.Potato) || other.gameObject.name == "Carrot_Projectile(Clone)" && curShield == shieldType.Carrot)
+        {
             Destroy(other.gameObject);
-        if (other.gameObject.name == "Carrot_Projectile(Clone)" && curShield == shieldType.Carrot)
-            Destroy(other.gameObject);
+            AudioSource[] sounds = GameController.GetComponents<AudioSource>();
+            sounds[2].Play();
+        }
     }
 }
