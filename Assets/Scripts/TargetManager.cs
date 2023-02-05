09e5edcd590e1da65afdc1c34be672da2554fc31
Hunter McConnell <huntmc@outlook.com>
2023-02-05 13:30:35 -0500

    Angry Potato Core
    Core of Angry Potato game done, just need to add game management and win con.

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public GameObject targetPrefab;
+    Rigidbody target;
+    Collider targetCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
