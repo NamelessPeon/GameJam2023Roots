511d8e1b96125c4b394d42db1dabb55b3b4eedba
NamelessPeon <robert63090@hotmail.com>
2023-02-04 21:40:18 -0500

    Rooting around bounded
    YEAAAAAAAAAAA

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         Destroy(this.gameObject);
     }
